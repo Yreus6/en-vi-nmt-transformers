fb46e2c7fd4ce5d568fc2c7dfdb8b32352f898f2
Yreus6 <wizard201366@gmail.com>
2023-10-25 13:23:51 +0700

    feat: improve performance when prepare data

--- helper/utils.py
+++ helper/utils.py
@@ -1,4 +1,6 @@
 import os
+from tqdm import tqdm
+import multiprocessing as mp
 
 
 class SaveHandle(object):
@@ -17,3 +19,49 @@ class SaveHandle(object):
             self.save_list.append(save_path)
             if os.path.exists(remove_path):
                 os.remove(remove_path)
+
+
+class MultiProcessorWriter:
+
+    def __init__(self, output_files, process_fn, listener_fn, data_len, batch_size):
+        manager = mp.Manager()
+        self.output_files = output_files
+        self.process_fn = process_fn
+        self.listener_fn = listener_fn
+        self.data_len = data_len
+        self.batch_size = batch_size
+        self.q = manager.Queue()
+
+    def worker(self, idx):
+        batch_res = self.process_fn(idx, self.batch_size, self.data_len)
+        self.q.put(batch_res)
+
+    def listener(self):
+        """listens for messages on the q"""
+        while 1:
+            m = self.q.get()
+            if m == 'kill':
+                break
+
+            self.listener_fn(m, self.output_files)
+
+    def run(self):
+        pool = mp.Pool(mp.cpu_count() + 2)
+        # put listener to work first
+        watcher = pool.apply_async(self.listener)
+
+        # fire off workers
+        jobs = []
+        for i in range(0, self.data_len, self.batch_size):
+            job = pool.apply_async(self.worker, (i,))
+            jobs.append(job)
+
+        # collect results from the workers through the pool result queue
+        for job in tqdm(jobs):
+            job.get()
+
+        # now we are done, kill the listener
+        self.q.put('kill')
+        watcher.get()
+        pool.close()
+        pool.join()
--- prepare_data.py
+++ prepare_data.py
@@ -1,34 +1,53 @@
 import os
 import argparse
 from datasets import load_dataset
-from tqdm import tqdm
 from underthesea import word_tokenize
+
+from helper.utils import MultiProcessorWriter
+
+
+def process_fn(idx, batch_size, data_len):
+    en_batch = ''
+    vi_batch = ''
+    for i in range(idx, idx + batch_size):
+        if i >= data_len:
+            break
+
+        prompt = dataset[phase][i]['prompt']
+        prompt_ques = prompt.split(':')[0].strip()
+        translation = dataset[phase][i]['translation']
+        if 'English' in prompt_ques or 'Anh' in prompt_ques:
+            en_sent = translation['target']
+            vi_sent = translation['source']
+        elif 'Vietnamese' in prompt_ques or 'Việt' in prompt_ques:
+            en_sent = translation['source']
+            vi_sent = translation['target']
+        else:
+            print(f'Error in data: {dataset[phase][i]}')
+            continue
+
+        en_batch += en_sent + '\n'
+        vi_batch += word_tokenize(vi_sent, format='text') + '\n'
+
+    return en_batch, vi_batch
+
+
+def listener_fn(s, output_files):
+    en_path, vi_path = output_files
+    with open(en_path, 'a', encoding='utf-8') as f_en, open(vi_path, 'a', encoding='utf-8') as f_vi:
+        f_en.write(s[0])
+        f_en.flush()
+
+        f_vi.write(s[1])
+        f_vi.flush()
 
 
 def generate_data(dataset, phase, output_path):
     en_path = os.path.join(output_path, f'{phase}.en.txt')
     vi_path = os.path.join(output_path, f'{phase}.vi.txt')
 
-    with open(en_path, 'w', encoding='utf-8') as f_en, open(vi_path, 'w', encoding='utf-8') as f_vi:
-        for i in tqdm(range(len(dataset[phase]))):
-            prompt = dataset[phase][i]['prompt']
-            prompt_ques = prompt.split(':')[0].strip()
-            translation = dataset[phase][i]['translation']
-            if 'English' in prompt_ques or 'Anh' in prompt_ques:
-                en_sent = translation['target']
-                vi_sent = translation['source']
-            elif 'Vietnamese' in prompt_ques or 'Việt' in prompt_ques:
-                en_sent = translation['source']
-                vi_sent = translation['target']
-            else:
-                print(f'Error in data: {dataset[phase][i]}')
-                continue
-
-            f_en.write(en_sent + '\n')
-            f_vi.write(word_tokenize(vi_sent, format='text') + '\n')
-
-        f_en.close()
-        f_vi.close()
+    writer = MultiProcessorWriter([en_path, vi_path], process_fn, listener_fn, len(dataset[phase]), 1000)
+    writer.run()
 
 
 if __name__ == '__main__':
